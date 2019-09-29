7ee49bc29456510e7014a1dbef4c3588afd2236c
Daniel Costa <dcostabus@gmail.com>
2019-09-29 19:49:43 -0300

    Adding first update for myPythonFunction.py, which centralizes all functions used in the program.

---
+++ myPythonFunction.py
@@ -0,0 +1,101 @@
+import random, os
+
+fileName = 'userScore.txt'
+tmpFile = 'userScore.tmp'
+
+try:
+    #searchUser = str(input("Insert the name that you want:"))
+
+    def getUserPoint(userName):
+        f = open(fileName, 'r')
+        results = f.readlines()
+    
+        counter = False
+        
+        for result in results:
+            splitado = result.split(',')
+            for innerResult in splitado:
+                if counter == True:
+                    print('Score: ',innerResult)
+                    break
+
+                if innerResult == userName:
+                    counter = True
+                    continue
+            if counter == True:
+                break
+                
+        if counter == False:
+            print("Sorry, name not found :( ")
+
+        f.close()
+
+    def updateUserPoints(newUser, userName, score):
+        if newUser == True:
+            f = open(fileName, 'a')
+            model = "\n{}, {}"
+            string = model.format(userName,score)
+            f.write(string)
+            f.close()
+        else:
+            inputFile = open(fileName, "r")
+            outputFile = open(tmpFile,"a")
+
+            lines = inputFile.readlines()
+            
+            arrayUsers = []
+            for line in lines:
+                user = line.split(',')
+                arrayUsers.append(user)
+
+            for idx, users in enumerate(arrayUsers):
+                if arrayUsers[idx][0] == userName:
+                    arrayUsers[idx][1] = str(score) #+ '\n'
+
+                string = "{}, {}\n"
+                stringLast = "{}, {}"
+                arrayLenght = len(arrayUsers)
+
+            for idx, users in enumerate(arrayUsers):
+                userArray = arrayUsers[idx][0]
+                scoreArray = arrayUsers[idx][1]
+                userArray = userArray.strip()
+                scoreArray = scoreArray.strip()
+                if (idx + 1) < arrayLenght:
+                    outputFile.write(string.format(userArray,scoreArray))
+                else:
+                    outputFile.write(stringLast.format(userArray,scoreArray))
+            
+            inputFile.close()
+            outputFile.close()
+
+            os.remove(fileName)
+            os.rename(tmpFile,fileName)
+
+    def showAllScore():
+        f = open(fileName, 'r')
+        stringScores ='''{}-{}-{}'''
+
+        lines = f.readlines()
+        arrayUsers = []
+        for line in lines:
+            user = line.split(',')
+            arrayUsers.append(user)
+        print(len(arrayUsers))
+        for idx, users in enumerate(arrayUsers):
+            userArray = arrayUsers[idx][0]
+            scoreArray = arrayUsers[idx][1]
+            userArray = userArray.strip()
+            scoreArray = scoreArray.strip()
+            print(stringScores.format(idx+1, userArray, scoreArray))
+
+        f.close()
+
+    #getUserPoint(searchUser)
+
+    #updateUserPoints(False, 'Damiao', 1055)
+
+except IOError:
+    print("Sorry, file not found! We will create one for you! Try again after")
+    f = open(fileName, 'w')
+    f.close()
