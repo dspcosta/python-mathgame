f2bef192e49ad309c6508b6024947f6afcf19fbd
dspcosta <dcostabus@gmail.com>
2019-09-30 13:54:58 -0300

    Adding PEP 8 style guide into written code

--- main.py
+++ main.py
@@ -6,36 +6,37 @@ try:
     finishProgram = False
     counter = 0
 
-    while finishProgram == False:
-    
+    while finishProgram is False:
+
         if counter > 0:
-            optionContinue = int(input('Do you want to do a new search or Update? Yes[1] No[2]:  '))
+            optionContinue = int(input(
+                'Do you want to do a new search or Update? Yes[1] No[2]:  '
+                ))
             if optionContinue == 2:
                 print("Thank you, see you soon!")
                 break
             elif optionContinue > 2:
                 print("Not a valid option, the program will be terminated.")
-                break 
+                break
 
         counter = counter + 1
 
         print('''
-        
-        ***************************************************
-        *                                                 *
-        *            WELCOME TO THE SCORE GAME            *
-        *                                                 *  
-        ***************************************************   
 
-        Please select one of the options below:
+            ***************************************************
+            *                                                 *
+            *            WELCOME TO THE SCORE GAME            *
+            *                                                 *
+            ***************************************************
 
-        1) Search for a Score from a Specific User Name
-        2) Update a Score for a specific User Name
-        3) Show All Scores
-        4) New Math Game!!!
-        5) Quit
-        '''
-        )
+            Please select one of the options below:
+
+            1) Search for a Score from a Specific User Name
+            2) Update a Score for a specific User Name
+            3) Show All Scores
+            4) New Math Game!!!
+            5) Quit
+            ''')
         optionSelect = int(input("Insert option:"))
 
         if optionSelect == 1:
@@ -45,14 +46,22 @@ try:
             optionUpdate = int(input("Is it a New User? Yes[1] No[2]"))
             if optionUpdate == 1:
                 newUser = True
-                searchUser = str(input("Insert the name that you want to insert Score:"))
+                searchUser = str(input(
+                    "Insert the name that you want to insert Score:"
+                    ))
                 scoreUpdate = int(input("Insert the Score:"))
-                myPythonFunction.updateUserPoints(newUser, searchUser, scoreUpdate)
+                myPythonFunction.updateUserPoints(
+                    newUser, searchUser, scoreUpdate
+                    )
             elif optionUpdate == 2:
                 newUser = False
-                searchUser = str(input("Insert the name that you want to update the Score:"))
+                searchUser = str(input(
+                    "Insert the name that you want to update the Score:"
+                    ))
                 scoreUpdate = int(input("Insert the New Score:"))
-                myPythonFunction.updateUserPoints(newUser, searchUser, scoreUpdate)
+                myPythonFunction.updateUserPoints(
+                    newUser, searchUser, scoreUpdate
+                    )
         elif optionSelect == 3:
             myPythonFunction.showAllScore()
         elif optionSelect == 4:
--- mathGame.py
+++ mathGame.py
@@ -1,43 +1,47 @@
 import random
 import myPythonFunction
 
+
 def mathGame():
     quitGame = False
 
-    while quitGame == False:
+    while quitGame is False:
         operandList = []
-        for x in range(0,5):
+        for x in range(0, 5):
             operandList = operandList + [x]
-            operandList[x] = random.randint(1,9)
+            operandList[x] = random.randint(1, 9)
 
         operatorDict = {
-            1:"+",
-            2:"-",
-            3:"*",
-            4:"**"
+            1: "+",
+            2: "-",
+            3: "*",
+            4: "**"
         }
 
         operatorList = []
-        for x in range(0,4):
+        for x in range(0, 4):
             operatorList = operatorList + [x]
-        
-            operatorList[x] = [operatorDict[random.randint(1,4)]]
+
+            operatorList[x] = [operatorDict[random.randint(1, 4)]]
 
             if operatorList[x - 1][0] == '**':
-                operatorList[x] = [operatorDict[random.randint(1,3)]]
-            
+                operatorList[x] = [operatorDict[random.randint(1, 3)]]
+
         equation = ' '
-        for x in range(1,6):
+        for x in range(1, 6):
             if x == 5:
-                eqpart = str(operandList[random.randint(0,4)])
+                eqpart = str(operandList[random.randint(0, 4)])
                 equation = equation + eqpart
                 break
 
-            eqpart = str(operandList[random.randint(0,4)]) + " " + str(operatorList[random.randint(0,3)][0]) + " "
+            eqpart = str(operandList[random.randint(0, 4)]) + " " + str(
+                operatorList[random.randint(0, 3)][0]
+                ) + " "
+
             equation = equation + eqpart
 
         result = eval(equation)
-        display = equation.replace("**","^")
+        display = equation.replace("**", "^")
 
         newUserQuestion = int(input('Is it a New User? Yes[1] No[2] '))
 
@@ -45,9 +49,11 @@ def mathGame():
 
             newScore = 0
 
-            userNameGame = input('Please insert the User Name that will play: ')
+            userNameGame = input(
+                'Please insert the User Name that will play: '
+                )
 
-            print("The equation to be solved is:\n",display)
+            print("The equation to be solved is:\n", display)
 
             valueAnswer = int(input('What is the Result? '))
 
@@ -63,9 +69,11 @@ def mathGame():
         else:
             newScore = 0
 
-            userNameGame = input('Please insert the User Name that will play: ')
+            userNameGame = input(
+                'Please insert the User Name that will play: '
+                )
 
-            print("The equation to be solved is:\n",display)
+            print("The equation to be solved is:\n", display)
 
             valueAnswer = int(input('What is the Result? '))
 
@@ -76,16 +84,18 @@ def mathGame():
             else:
                 print('Wrong answer!! The correct result is: ', result)
                 newScore = 0
-            
+
             oldScore = int(myPythonFunction.setUserPoint(userNameGame))
             updateScore = oldScore + newScore
             myPythonFunction.updateUserPoints(False, userNameGame, updateScore)
 
-        quitOrNo = int(input('Do you want to continue in the Game? Yes[1] No[2] '))
+        quitOrNo = int(input(
+            'Do you want to continue in the Game? Yes[1] No[2] '
+            ))
 
         if quitOrNo == 1:
             quitGame = False
-            
+
         else:
             quitGame = True
             print("Thank you, see you later!")
