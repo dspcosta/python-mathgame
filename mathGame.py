1578127b0b039a69972fdec2e1f641203026fdf9
Daniel <contingenciamento01@segess.local>
2019-09-30 11:16:31 -0300

    Update Math Game file, including game functionalities and input verifications

--- mathGame.py
+++ mathGame.py
@@ -1,40 +1,91 @@
 import random
+import myPythonFunction
 
-operandList = []
-for x in range(0,5):
-    operandList = operandList + [x]
-    operandList[x] = random.randint(1,9)
+def mathGame():
+    quitGame = False
 
-operatorDict = {
-    1:"+",
-    2:"-",
-    3:"*",
-    4:"**"
-}
+    while quitGame == False:
+        operandList = []
+        for x in range(0,5):
+            operandList = operandList + [x]
+            operandList[x] = random.randint(1,9)
 
-operatorList = []
-for x in range(0,4):
-    operatorList = operatorList + [x]
- 
-    operatorList[x] = [operatorDict[random.randint(1,4)]]
+        operatorDict = {
+            1:"+",
+            2:"-",
+            3:"*",
+            4:"**"
+        }
 
-    if operatorList[x - 1][0] == '**':
-         operatorList[x] = [operatorDict[random.randint(1,3)]]
-    
+        operatorList = []
+        for x in range(0,4):
+            operatorList = operatorList + [x]
+        
+            operatorList[x] = [operatorDict[random.randint(1,4)]]
 
-print(operatorDict)
-print(operandList)
-print(operatorList)
+            if operatorList[x - 1][0] == '**':
+                operatorList[x] = [operatorDict[random.randint(1,3)]]
+            
+        equation = ' '
+        for x in range(1,6):
+            if x == 5:
+                eqpart = str(operandList[random.randint(0,4)])
+                equation = equation + eqpart
+                break
 
-equation = ' '
-for x in range(1,6):
-    if x == 5:
-        eqpart = str(operandList[random.randint(0,4)])
-        equation = equation + eqpart
-        break
+            eqpart = str(operandList[random.randint(0,4)]) + " " + str(operatorList[random.randint(0,3)][0]) + " "
+            equation = equation + eqpart
 
-    eqpart = str(operandList[random.randint(0,4)]) + " " + str(operatorList[random.randint(0,3)][0]) + " "
-    equation = equation + eqpart
+        result = eval(equation)
+        display = equation.replace("**","^")
 
-print(equation)
-print(eval(equation))
+        newUserQuestion = int(input('Is it a New User? Yes[1] No[2] '))
+
+        if newUserQuestion == 1:
+
+            newScore = 0
+
+            userNameGame = input('Please insert the User Name that will play: ')
+
+            print("The equation to be solved is:\n",display)
+
+            valueAnswer = int(input('What is the Result? '))
+
+            if valueAnswer == int(result):
+                print('Right answer!! :)')
+                newScore = 50
+
+            else:
+                print('Wrong answer!! The correct result is: ', result)
+                newScore = 0
+
+            myPythonFunction.updateUserPoints(True, userNameGame, newScore)
+        else:
+            newScore = 0
+
+            userNameGame = input('Please insert the User Name that will play: ')
+
+            print("The equation to be solved is:\n",display)
+
+            valueAnswer = int(input('What is the Result? '))
+
+            if valueAnswer == int(result):
+                print('Right answer!! :) You got 50 Points!')
+                newScore = 50
+
+            else:
+                print('Wrong answer!! The correct result is: ', result)
+                newScore = 0
+            
+            oldScore = int(myPythonFunction.setUserPoint(userNameGame))
+            updateScore = oldScore + newScore
+            myPythonFunction.updateUserPoints(False, userNameGame, updateScore)
+
+        quitOrNo = int(input('Do you want to continue in the Game? Yes[1] No[2] '))
+
+        if quitOrNo == 1:
+            quitGame = False
+            
+        else:
+            quitGame = True
+            print("Thank you, see you later!")
